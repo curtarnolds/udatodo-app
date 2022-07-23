dcfce301279dcc8a78a1c52bdd70b975dea86ca7
curtarnold <afayedormawuli@gmail.com>
2022-07-23 15:19:34 +0000

    first commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = ' 836ftcyztc';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/prod`
+
+export const authConfig = {
+    domain: 'curtarnolds.us.auth0.com',
+    clientId: 'TbqsJBnkguBbk77lmDwQiYJiQZULAbqx',
+    callbackUrl: 'http://localhost:3000/callback'
+};
